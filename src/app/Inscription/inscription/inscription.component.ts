79cf700fde28d0f212a2c1273c0742d8c0443d63
Ibrahim Djingareye <maigaib82@gmail.com>
2023-08-28 23:50:26 +0000

    commit without services added

--- src/app/Inscription/inscription/inscription.component.ts
+++ src/app/Inscription/inscription/inscription.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
-export class InscriptionComponent {
+export class InscriptionComponent implements OnInit {
+  constructor(private router: Router) { }
+  ngOnInit(): void { }
+
+    onSave(): void{
+      this.router.navigateByUrl('projet');
+    }
+      
+ 
+ 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { AppComponent } from './app.component';
 
 import { DetailsTacheComponent } from './details-tache/details-tache.component';
 import { GestionMembresComponent } from './gestion-membres/gestion-membres.component';
-
-import { InscriptionComponent } from './Inscription/inscription/inscription.component';
 import { HeaderComponent } from './header/header.component';
 import { CreerProjetComponent } from './creer-projet/creer-projet.component';
-import { TableauDeBordProjetComponent } from './tableau-de-bord-projet/tableau-de-bord-projet/tableau-de-bord-projet.component';
+
 import { ListeTachesComponent } from './liste-taches/liste-taches/liste-taches.component';
+import { InscriptionComponent } from './Inscription/inscription/inscription.component';
+import { TableauDeBordProjetComponent } from './tableau-de-bord-projet/tableau-de-bord-projet/tableau-de-bord-projet.component';
 
 @NgModule({
   declarations: [
@@ -27,12 +27,13 @@ import { ListeTachesComponent } from './liste-taches/liste-taches/liste-taches.c
   ],
   imports: [
     RouterModule.forRoot([
-      {path: '', component: TableauDeBordProjetComponent},
+      {path: '', component: InscriptionComponent},
       {path: 'creer-projet', component: CreerProjetComponent},
       {path: 'liste-taches', component: ListeTachesComponent},
       {path:'details-tache', component: DetailsTacheComponent},
       {path: 'gestion-membres', component: GestionMembresComponent},
-      {path: '**', component: TableauDeBordProjetComponent}
+      //{path: '**', component: InscriptionComponent},
+      {path:'projet', component:TableauDeBordProjetComponent}
     ]),
     BrowserModule,
     AppRoutingModule,
