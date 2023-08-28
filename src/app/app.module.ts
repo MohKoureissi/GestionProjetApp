6c546ab19a92a608744efb53248b8bb0119277cd
MohKoureissi <koureissi89@gmail.com>
2023-08-28 09:32:40 +0000

    3 eme commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { TableauDeBordProjetComponent } from './tableau-de-bord-projet/tableau-d
 import { ListeTachesComponent } from './liste-taches/liste-taches.component';
 import { DetailsTacheComponent } from './details-tache/details-tache.component';
 import { GestionMembresComponent } from './gestion-membres/gestion-membres.component';
+import { CreerProjetComponent } from './creer-projet/creer-projet.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +16,22 @@ import { GestionMembresComponent } from './gestion-membres/gestion-membres.compo
     TableauDeBordProjetComponent,
     ListeTachesComponent,
     DetailsTacheComponent,
-    GestionMembresComponent
+    GestionMembresComponent,
+    CreerProjetComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {path: '', component: TableauDeBordProjetComponent},
+      {path: 'creer-projet', component: CreerProjetComponent},
+      {path: 'liste-taches', component: ListeTachesComponent},
+      {path:'details-tache', component: DetailsTacheComponent},
+      {path: 'gestion-membres', component: GestionMembresComponent},
+      {path: '**', component: TableauDeBordProjetComponent}
+    ]),
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    AppRoutingModule,
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
